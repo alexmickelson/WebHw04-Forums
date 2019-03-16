ca71bfb82f9eb2c1f182c00fff496c72bbdcd399
alexmickelson96 <alexmickelson96@gmail.com>
2019-03-16 13:23:19 -0600

    worked

---
+++ WebHw04-Forums/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebHw04_Forums.Data;
+
+namespace WebHw04_Forums.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdminController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebHw04-Forums/Controllers/CommentsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebHw04_Forums.Data;
+using WebHw04_Forums.Models;
+
+namespace WebHw04_Forums.Controllers
+{
+    public class CommentsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public CommentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Comment>> getComments()
+        {
+            return await _context.Comment.ToListAsync();
+        }
+
+        public async Task<List<Comment>> getComments(int postId, int? parentComment)
+        {
+            return await _context.Comment.Where(c => c.ParentId == parentComment && c.PostId == postId).ToListAsync();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,Time,ParentId,Content,PostId")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Details", "Posts", new { id = comment.PostId });
+            }
+            return View(comment);
+        }
+    }
+}
